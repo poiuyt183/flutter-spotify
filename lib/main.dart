6a32b7ef054a1975212374d19c600f3b6b4d3ecc
khuyentv <khuyentv@khuyentvs-Mac-mini.local>
2026-01-14 15:08:35 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:spotify/core/configs/theme/app_theme.dart';
+import 'package:spotify/presentation/splash/pages/splash.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppTheme.lightTheme,
+      debugShowCheckedModeBanner: false,
+      home: const SplashPage(),
+    );
+  }
+}
